c76164a24db6297d8f7cf85dfcbeb3cb2ab40c7b
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2021-03-04 21:43:23 +0300

    Start

---
+++ src/main/java/by/grodno/duba/site/openexcel/Starter.java
@@ -0,0 +1,11 @@
+package by.grodno.duba.site.openexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+}
