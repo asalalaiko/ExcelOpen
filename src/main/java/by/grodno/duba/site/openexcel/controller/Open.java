cfc66618b1cd5ac13bfd40d73f41917a49e66876
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2021-03-11 23:00:38 +0300

    openfile

---
+++ src/main/java/by/grodno/duba/site/openexcel/controller/Open.java
@@ -0,0 +1,55 @@
+package by.grodno.duba.site.openexcel.controller;
+
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+@Controller
+public class Open {
+    @GetMapping("/open")
+    public static void main(String[] args) throws IOException {
+        String excelFilePath = "Books.xlsx";
+        FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet firstSheet = workbook.getSheetAt(0);
+        Iterator<Row> iterator = firstSheet.iterator();
+
+        while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+
+
+                System.out.print(cell.getCellType());
+
+//                switch (cell.getCellType()) {
+//                    case CellType.STRING:
+//                        System.out.print(cell.getStringCellValue());
+//                        break;
+//                    case Cell.CELL_TYPE_BOOLEAN:
+//                        System.out.print(cell.getBooleanCellValue());
+//                        break;
+//                    case Cell.CELL_TYPE_NUMERIC:
+//                        System.out.print(cell.getNumericCellValue());
+//                        break;
+//                }
+                System.out.print(" - ");
+            }
+            System.out.println();
+        }
+
+        workbook.close();
+        inputStream.close();
+    }
+}
