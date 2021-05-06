9fad5614e27f83e072f7618c026b66a7cd1efceb
asalalaiko <a.salalaiko@gmail.com>
2021-05-06 14:21:41 +0300

    06/05
    add ExcelPOIHelper

--- src/main/java/by/grodno/duba/site/openexcel/controller/Open.java
+++ src/main/java/by/grodno/duba/site/openexcel/controller/Open.java
@@ -8,55 +8,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.*;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import by.grodno.duba.site.openexcel.poi.excel.ExcelPOIHelper;
 
 
 @Controller
 public class Open {
+    private static ExcelPOIHelper excelPOIHelper;
+
     @GetMapping("/open")
-    public static void main(String[] args, Model model) throws IOException {
+    public static String main(Model model) throws IOException {
         String excelFilePath = "Test.xlsx";
         FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+        Object cellValue = new Object();
 
         Workbook workbook = new XSSFWorkbook(inputStream);
         Sheet firstSheet = workbook.getSheetAt(0);
         Iterator<Row> iterator = firstSheet.iterator();
 
-        model.addAttribute("cell", iterator);
+
+        if (excelFilePath != null) {
+            if (excelFilePath.endsWith(".xlsx") || excelFilePath.endsWith(".xls")) {
 
 
-
-       while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
-            model.addAttribute("cellIterator", cellIterator);
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                }}
-/*
-                     System.out.print(cell.getStringCellValue());
-
-//                switch (cell.getCellType()) {
-//                    case CellType.STRING:
-//                        System.out.print(cell.getStringCellValue());
-//                        break;
-//                    case Cell.CELL_TYPE_BOOLEAN:
-//                        System.out.print(cell.getBooleanCellValue());
-//                        break;
-//                    case Cell.CELL_TYPE_NUMERIC:
-//                        System.out.print(cell.getNumericCellValue());
-//                        break;
-//                }
-                System.out.print(" - ");
+                Map<Integer, List<String>> data
+                        = excelPOIHelper.readExcel(excelFilePath);
+                model.addAttribute("data", data);
+            } else {
+                model.addAttribute("message", "Not a valid excel file!");
             }
-            System.out.println();
+        } else {
+            model.addAttribute("message", "File missing! Please upload an excel file.");
         }
+        return "excel";
+    }
 
-        workbook.close();
-        inputStream.close();
-*/
+    public class MyCell {
+        private String content;
+        private String textColor;
+        private String bgColor;
+        private String textSize;
+        private String textWeight;
+
+        public MyCell(String content) {
+            this.content = content;
+        }
     }
 }
+
+
